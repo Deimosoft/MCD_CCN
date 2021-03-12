45cac31d1bf265ba301b48447cdc12fccd7b18a2
hydrajake <80480036+hydrajake@users.noreply.github.com>
2021-03-12 15:29:40 -0500

    Update installCD.sh

--- installCD.sh
+++ installCD.sh
@@ -1,41 +1,31 @@
 #Download and install pi_video_looper
 echo "Let's get started!"
 echo "~~~~~~~~~~~~~~~~~~"
-echo "Downloading the loopersystem."
+echo "Installing the loopersystem."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd
-sudo git clone https://github.com/adafruit/pi_video_looper.git
-cd pi_video_looper
-rm -r assests/video_looper.ini #Deletes the configuration file included.
-cp ../MCD_CCN/video_looper.ini assets/video_looper.ini #Installs a new configuration file set with new file paths and suggested settings.
 sudo ./install.sh #Installs the looper
 
 #Build CCD folder system
 echo "Making the CCD file system"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd
-sudo mkdir CCD
-sudo mkdir CCD/Videos
-sudo mkdir CCD/Videos/Temp
-sudo mkdir CCD/Videos/Ready
-sudo mkdir CCD/Scripts
-sudo mkdir CCD/Scripts/Commands
-
-#Move scripts into the sripts drive
-echo "Move pre-made scripts over"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd
-cd MCD_CCN
-cp StartC ~/CCD/Scripts/Commands
-cp StopC ~/CCD/Scripts/Commands
+sudo mkdir ~/CCD
+sudo mkdir ~/CCD/Videos
+sudo mkdir ~/CCD/Videos/Temp
+sudo mkdir ~/CCD/Videos/Ready
+sudo mkdir ~/CCD/Scripts
+sudo mkdir ~/CCD/Scripts/Commands
 
 #Make scripts executable
 echo "Make the scripts executeable."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-cd
-cd CCD/Scripts/Commands
 sudo chmod +x StartC
 sudo chmod +x StopC
+
+#Move scripts into the sripts drive
+echo "Move pre-made scripts over"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+cp StartC ~/CCD/Scripts/Commands
+cp StopC ~/CCD/Scripts/Commands
 
 #Move scripts into /bin
 echo "Moving the scripts into the /bin folder."
@@ -56,8 +46,7 @@ sudo cp StopC /bin
 
 #Edit MOTD
 sudo rm -r /etc/motd
-cd
-sudo cp MCD_CCN/motd /etc
+sudo cp motd /etc
 
 #Finish
 echo "Enjoy!"
