8bb158bb2c55777e9ebcaed157a490cf0f64f376
hydrajake <80480036+hydrajake@users.noreply.github.com>
2021-03-16 09:46:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ cp ./assets/video_looper.ini /boot/video_looper.ini
 echo "Configuring video_looper to run on start..."
 echo "==========================================="
 
-cp ./assets/video_looper.conf /etc/supervisor/conf.d/
+cp ./assets/video_looper.conf /etc/supervisor/conf.d
 
 service supervisor restart
 
